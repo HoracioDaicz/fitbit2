2571468523a7ff2b0049ab6b485a0ab2f7294aa1
HoracioDaicz <100947653+HoracioDaicz@users.noreply.github.com>
2022-05-03 12:22:08 -0300

    Update boot.js

--- boot.js
+++ boot.js
@@ -2,7 +2,7 @@
 var fitbitAccessToken;
 
 if (!window.location.hash) {
-    window.location.replace('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=227H22&redirect_uri=https%3A%2F%2F15359f83.ngrok.io%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');
+    window.location.replace('window.location.replace('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=238BW6&redirect_uri=https://horaciodaicz.github.io/fitbit/&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');');
 } else {
     var fragmentQueryParameters = {};
     window.location.hash.slice(1).replace(
